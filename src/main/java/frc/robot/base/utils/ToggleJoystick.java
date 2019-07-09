90014182843772d2c8d8d91df1bb5c4a3e80402d
Nadav <tashernadav@gmail.com>
2019-07-09 15:10:41 +0300

    Code Redesign Simple doc for ToggleJoystick

--- src/main/java/frc/robot/base/utils/ToggleJoystick.java
+++ src/main/java/frc/robot/base/utils/ToggleJoystick.java
@@ -8,16 +8,19 @@ import frc.robot.base.Module;
  * https://github.com/GeneralAngels/RIO20
  */
 
+// This class takes a joystick and maps it to many Toggle classes.
+// Gives us the ability to use all of the physical-buttons as toggles or triggers.
+
 public class ToggleJoystick extends Module {
 
     private Joystick actual;
     private Toggle trigger, sidebutton;
 
-    public ToggleJoystick(Joystick actual){
+    public ToggleJoystick(Joystick actual) {
         this.actual = actual;
     }
 
-    public void update(){
+    public void update() {
         trigger.update(actual.getTrigger());
     }
 }
