5e2779a57e5c82f5464a462dcaa959ec11e0ae61
Nadav <tashernadav@gmail.com>
2019-07-09 14:04:27 +0300

    Code Redesign Odometry

--- src/main/java/frc/robot/base/drive/Odometry.java
+++ src/main/java/frc/robot/base/drive/Odometry.java
@@ -5,93 +5,77 @@ import org.json.JSONObject;
 
 public class Odometry extends Module {
 
-    public static final String X = "x", Y = "y", THETA = "theta", LINEAR = "linear_velocity", ANGULAR = "angular_velocity", LEFT_SETPOINT = "left_setpoint", RIGHT_SETPOINT = "right_setpoint";
-
     private double x, y, theta, linear, angular, rightSetpoint, leftSetpoint, distance;
 
     public double getRightSetpoint() {
         return rightSetpoint;
     }
 
-    public Odometry setRightSetpoint(double rightSetpoint) {
+    public void setRightSetpoint(double rightSetpoint) {
         this.rightSetpoint = rightSetpoint;
-        return this;
+        log("setpoint-right", rightSetpoint);
     }
 
     public double getLeftSetpoint() {
         return leftSetpoint;
     }
 
-    public Odometry setLeftSetpoint(double leftSetpoint) {
+    public void setLeftSetpoint(double leftSetpoint) {
         this.leftSetpoint = leftSetpoint;
-        return this;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public Odometry setX(double x) {
-        this.x = x;
-        return this;
+        log("setpoint-left", leftSetpoint);
     }
 
     public double getDistance() {
         return distance;
     }
 
-    public Odometry setDistance(double distance) {
+    public void setDistance(double distance) {
         this.distance = distance;
-        return this;
+        log("distance", distance);
     }
 
     public double getLinear() {
         return linear;
     }
 
-    public Odometry setLinear(double linear) {
+    public void setLinear(double linear) {
         this.linear = linear;
-        return this;
+        log("linear", linear);
     }
 
     public double getAngular() {
         return angular;
     }
 
-    public Odometry setAngular(double angular) {
+    public void setAngular(double angular) {
         this.angular = angular;
-        return this;
+        log("angular", angular);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+        log("x", x);
     }
 
     public double getY() {
         return y;
     }
 
-    public Odometry setY(double y) {
+    public void setY(double y) {
         this.y = y;
-        return this;
+        log("y", y);
     }
 
     public double getTheta() {
         return theta;
     }
 
-    public Odometry setTheta(double theta) {
+    public void setTheta(double theta) {
         this.theta = theta;
-        return this;
-    }
-
-    @Override
-    public JSONObject toJSON() {
-        JSONObject odometry = new JSONObject();
-        odometry.put(RIGHT_SETPOINT, getRightSetpoint());
-        odometry.put(LEFT_SETPOINT, getLeftSetpoint());
-        odometry.put(THETA, getTheta());
-        odometry.put(X, getX());
-        odometry.put(Y, getY());
-        odometry.put(LINEAR, getLinear());
-        odometry.put(ANGULAR, getAngular());
-        odometry.put("distance", distance);
-        return odometry;
+        log("theta", theta);
     }
 }
