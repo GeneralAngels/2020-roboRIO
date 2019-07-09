43ce9d7f53347609d6ba1454675f1fbb70db4f07
Nadav <tashernadav@gmail.com>
2019-07-09 13:16:03 +0300

    Code Redesign Bot

--- src/main/java/frc/robot/base/Bot.java
+++ src/main/java/frc/robot/base/Bot.java
@@ -1,59 +1,29 @@
 package frc.robot.base;
 
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import frc.robot.base.communications.Communicator;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 public class Bot extends Module {
-    public static final String TIME = "time";
+    private static final String TIME = "time";
 
-    protected NetworkTableInstance nti;
-    protected NetworkTable database;
-    protected NetworkTableEntry json;
-    protected Communicator communicator;
-
-    protected ArrayList<Module> modules = new ArrayList<>();
-
-    protected void addToJSON(Module module) {
-        modules.add(module);
-    }
+    private Communicator communicator = null;
 
     public void init() {
         communicator = new Communicator(this);
     }
 
     public void teleop() {
-        loop();
+        if (communicator != null) communicator.update();
     }
 
     public void autonomous() {
-        loop();
-    }
-
-    protected void loop() {
-        json.setString(toJSON().toString());
+        if (communicator != null) communicator.update();
     }
 
     @Override
-    public JSONObject toJSON() {
-        JSONObject returnObject = super.toJSON();
-        returnObject.put(TIME, millis());
-        try {
-            for (Module module : modules) {
-                try {
-                    returnObject.put(module.getName().toLowerCase(), module.toJSON());
-                } catch (Exception ignored) {
-                }
-            }
-        } catch (Exception ignored) {
-        }
-        return returnObject;
-    }
-
-    protected void handleJSON(JSONObject object) {
+    public JSONObject pullJSON() {
+        JSONObject moduleJSON = super.pullJSON();
+        moduleJSON.put(TIME, millis());
+        return moduleJSON;
     }
 }
